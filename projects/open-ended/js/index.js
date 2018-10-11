1b1d3a3166a0486e7ebebb42846d726b678913e1
Maya <maya.arguell@gmail.com>
2018-10-11 12:41:30 -0400

    setup js file

---
+++ projects/open-ended/js/index.js
@@ -0,0 +1,19 @@
+
+
+function preload() {
+    
+}
+
+function setup() {
+    
+}
+
+function draw() {
+    sceneHandler( globalState );
+}
+
+function sceneHandler( globalState ) {
+    switch ( globalState ) {
+            
+    }
+}
